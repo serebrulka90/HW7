9a2f2749d4da954f08fb6aaf3dea03edc2f89f18
AlonaS <serebrulka90@gmail.com>
2020-02-18 15:29:59 +0200

    функция которая будет считать сколько раз символ встречается в строке

---
+++ Denis_gives_good_tasks.py
@@ -0,0 +1,5 @@
+def countSymbol(text, symbol):
+  return text.count(symbol)
+
+count = countSymbol('Денис даёт нам классные задачки, которые помогут нам найти лучшую работу!', 'е')
+print("Count: ", count)
